a9bc900735b398085fe44197d4f9e819e1b57f39
romachi <cookdon3@gmail.com>
2016-12-16 18:39:08 +0900

    44. 係り受け木の可視化

---
+++ sec5/nlp44.py
@@ -0,0 +1,20 @@
+from graphviz import Digraph
+from nlp41 import morphChunk
+
+
+def genNode(sentenceNum=5):
+    G = Digraph(format='png')
+    G.attr('node', shape='circle')
+
+    for chunk in morphChunk()[sentenceNum]:
+        s = ''.join([morph.surface for morph in chunk.morphs])
+        if chunk.dst != -1:
+            l = ''.join([morph.surface for morph in morphChunk()[sentenceNum][chunk.dst].morphs])
+            G.edge(s, l)
+
+    print(G)
+    G.render('nlp44_sentence' + str(sentenceNum))
+
+
+if __name__ == '__main__':
+    genNode(7)
