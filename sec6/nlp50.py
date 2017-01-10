842fa1bed181daf689c38125b1c041048c154d6c
romachi <cookdon3@gmail.com>
2017-01-11 00:31:03 +0900

    50. 文区切り

--- sec6/nlp50.py
+++ sec6/nlp50.py
@@ -1,10 +1,21 @@
 import re
 
+
 def separate_by_sentence(filename='nlp.txt'):
     pattern = re.compile(r"(^.*?[\.|\;|\:|\!|\?])\s([A-Z].*)")
     with open(filename) as f:
         for line in f:
+            line = line.strip()
+            while len(line) > 0:
+                match = pattern.match(line)
+                if match:
+                    yield match.group(1)
+                    line = match.group(2)
+                else:
+                    yield line
+                    line = ''
 
 
 if __name__ == '__main__':
-    separate_by_sentence()
+    for line in separate_by_sentence():
+        print(line)
