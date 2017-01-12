ff9283524a81a441c4eda01bee4ff33764809521
romachi <cookdon3@gmail.com>
2017-01-12 17:41:48 +0900

    変更後

--- sec6/nlp52.py
+++ sec6/nlp52.py
@@ -1,17 +1,12 @@
 from nltk import stem
 from nlp50 import separate_by_sentence
+from nlp51 import separate_by_space
 import re
-
-def add_stem(sentence):
-    words = sentence.split()
-    for word in words:
-        word_mod = re.sub(r'!', '', word)
-        yield word_mod + '\t' + stemmer.stem(word_mod)
-
 
 if __name__ == '__main__':
     stemmer = stem.PorterStemmer()
     for sentence in separate_by_sentence():
-        for word in add_stem(sentence):
-            print(word)
+        for word in separate_by_space(sentence):
+            word_mod = re.sub(r'[^a-z]', '', word.lower())
+            print(word + '\t' + stemmer.stem(word_mod))
         print('')
