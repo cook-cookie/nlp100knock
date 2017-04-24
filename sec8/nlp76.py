610626c95538c87c7ecca5956f3d562b39f57d41
romachi <cookdon3@gmail.com>
2017-04-24 11:33:17 +0900

    borderを引数に追加

--- sec8/nlp76.py
+++ sec8/nlp76.py
@@ -3,21 +3,22 @@ import pickle
 import math
 
 
-def prediction_enhanced(sentence: str, s: dict) -> tuple:
+def prediction_enhanced(sentence: str, s: dict, border: float) -> tuple:
     temp_list = sentence.split()
     temp_num = 0
     for word in temp_list[1:]:
         temp_num += s[word]
     g = (1. / (1. + math.exp(-temp_num)))
 
-    return '+1' if temp_list[0] == '1' else '-1', '+1' if g >= 0.5 else '-1', g
+    return '+1' if temp_list[0] == '1' else '-1', '+1' if g >= border else '-1', g
 
 
 if __name__ == '__main__':
     s = pickle.load(open('nlp73_pickle', 'rb'))
+    border = 0.5
 
     with open('features_mod.txt') as f:
         fi = f.readlines()
 
     for sentence in fi:
-        print(*prediction_enhanced(sentence, s), sep='\t')
+        print(*prediction_enhanced(sentence, s, border), sep='\t')
