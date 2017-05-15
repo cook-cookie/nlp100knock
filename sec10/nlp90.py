70bc2c436b4281a377b364bf3f8764fd95438fb1
romachi <cookdon3@gmail.com>
2017-05-16 01:00:44 +0900

    ファイルを作成

---
+++ sec10/nlp90.py
@@ -0,0 +1,9 @@
+from gensim.models import word2vec
+
+fname_input = '../sec9/nlp81.txt'
+data = word2vec.Text8Corpus(fname_input)
+model = word2vec.Word2Vec(data, size=200)
+
+out = model.most_similar(positive=[u''])
+for x in out:
+    print(x[0], x[1])
