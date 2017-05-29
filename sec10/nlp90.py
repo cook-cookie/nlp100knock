4b8f37f87e795f25f92ff185c25deceafde957b1
romachi <cookdon3@gmail.com>
2017-05-29 10:49:00 +0900

    ファイルを作成した

--- sec10/nlp90.py
+++ sec10/nlp90.py
@@ -2,8 +2,8 @@ from gensim.models import word2vec
 
 fname_input = '../sec9/nlp81.txt'
 data = word2vec.Text8Corpus(fname_input)
-model = word2vec.Word2Vec(data, size=200)
+model = word2vec.Word2Vec(data, size=300)
 
-out = model.most_similar(positive=[u''])
+out = model.most_similar(positive=[u'America'])
 for x in out:
     print(x[0], x[1])
